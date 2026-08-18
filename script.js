513bf57409a83984db7032830a54b6f2c08e57cc
Ekramul <ekramulislamworkonly@gmail.com>
2026-08-18 14:49:55 +0530

    Remove local songs to test streaming

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ let player = new Audio();
 let currentSongIndex = -1;
 
 const songs = [
-    { title: "Aawaz Deke Humen Tum Bulao", file: "90song/Aawaz Deke Humen Tum Bulao_320(KoshalWorld.Com).mp3" },
-    { title: "Bedardi Tere Pyar Ne Deewana Kar Diya", file: "90song/Bedardi Tere Pyar Ne Deewana Kar Diya_320(KoshalWorld.Com).mp3" },
-    { title: "Dekho Maine Dekha Hai Ye Ek Sapna", file: "90song/Dekho Maine Dekha Hai Ye Ek Sapna_320(KoshalWorld.Com).mp3" },
-    { title: "Tumne Kisi Se Kabhi Pyar Kiya Hai", file: "90song/Tumne Kisi Se Kabhi Pyar Kiya Hai_320(KoshalWorld.Com).mp3" }
+    { title: "Chithi Na Koi Sandesh (Streaming)", file: "https://koshalworld.com/files/download/type/320/id/37605" }
 ];
 
 let progressInterval;
